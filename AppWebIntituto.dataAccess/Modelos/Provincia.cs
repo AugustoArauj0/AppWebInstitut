794513d5330e75265f7fa0700ed3443f5ee98915
AugustoArauj0 <106758962+AugustoArauj0@users.noreply.github.com>
2022-06-07 16:45:07 -0300

    Update Provincia.cs

--- AppWebIntituto.dataAccess/Modelos/Provincia.cs
+++ AppWebIntituto.dataAccess/Modelos/Provincia.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,15 +10,17 @@ namespace AppWebInstitut.DataAccess.Modelos
     public class Provincia
     {
         [StringLength(50, ErrorMessage = "El largo máximo es de 50 caracteres")]
+        [DataType(DataType.Text)]
+        public int ProvinciaId { get; set; }
+
+        [StringLength(50, ErrorMessage = "El largo máximo es de 50 caracteres")]
+        [DataType(DataType.Text)]
+        public string? NombreProvincia { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MMMM/yyyy}")]
-        public int ProvinciaId { get; set; }
-        public string? NombreProvincia { get; set; }
         public DateTime FechaActualizacion { get; set; }
-        public string ObtenerProvincia()
-        {
-            return NombreProvincia;
-        }
+
     }
 }
 
