21dc4cfef135a1859d542a4ffc419d32a0b193af
AugustoArauj0 <106758962+AugustoArauj0@users.noreply.github.com>
2022-06-07 16:43:32 -0300

    Update Domicilio.cs

--- AppWebIntituto.dataAccess/Modelos/Domicilio.cs
+++ AppWebIntituto.dataAccess/Modelos/Domicilio.cs
@@ -1,53 +1,44 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.ComponentModel.DataAnnotations;
 
 namespace AppWebInstitut.DataAccess.Modelos
 {
     public class Domicilio
     {
-        [StringLength(50, ErrorMessage = "El largo máximo es de 50 caracteres")]
+        [StringLength(45, ErrorMessage = "El largo máximo es de 45 caracteres")]
+        [DataType(DataType.Text)]
+        public int DomicilioId { get; set; }
+
+        [StringLength(45, ErrorMessage = "El largo máximo es de 45 caracteres")]
+        [DataType(DataType.Text)]
+        public string? Calle { get; set; }
+
+        [StringLength(4, ErrorMessage = "El largo máximo es de 4 caracteres")]
+        [RegularExpression("([0-9]+)")]
+        public int Numero { get; set; }
+
+        [StringLength(3, ErrorMessage = "El largo máximo es de 3 caracteres")]
+        [RegularExpression("([0-9]+)")]
+        public int Piso { get; set; }
+
+        [StringLength(45, ErrorMessage = "El largo máximo es de 45 caracteres")]
+        [DataType(DataType.Text)]
+        public string? Departamento { get; set; }
+
+        [StringLength(45, ErrorMessage = "El largo máximo es de 45 caracteres")]
+        [DataType(DataType.Text)]
+        public string? Barrio { get; set; }
+
+        [StringLength(45, ErrorMessage = "El largo máximo es de 45 caracteres")]
+        [DataType(DataType.Text)]
+        public string? Manzana { get; set; }
+
+        [StringLength(45, ErrorMessage = "El largo máximo es de 45 caracteres")]
+        [DataType(DataType.Text)]
+        public int Casa { get; set; }
+
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString = "{0:dd/MMMM/yyyy}")]
-        public int DomicilioId { get; set; }
-        public string? Calle { get; set; }
-        public int Numero { get; set; }
-        public int Piso { get; set; }
-        public string? Departamento { get; set; }
-        public string? Barrio { get; set; }
-        public string? Manzana { get; set; }
-        public int Casa { get; set; }
         public DateTime FechaActualizacion { get; set; }
-        public string ObtenerCalle()
-        {
-            return Calle;
-        }
-        public int ObtenerNroCalle()
-        {
-            return Numero;
-        }
-        public int ObtenerPiso()
-        {
-            return Piso;
-        }
-        public string ObtenerDepartamento()
-        {
-            return Departamento;
-        }
-        public string ObtenerBarrio()
-        {
-            return Barrio;
-        }
-        public string ObtenerManzana()
-        {
-            return Manzana;
-        }
-        public int ObtenerCasa()
-        {
-            return Casa;
-        }
+
     }
 }
